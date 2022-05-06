2fada03853dfbc39e2d9ee01006beb499b279706
iceyheart <40313247+iceyheart@users.noreply.github.com>
2022-05-06 18:00:50 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd openwrt
 # wireless
-#rm -rf files/etc/config/wireless
-#rm -rf files/etc/modules.d/wireless_enable
+rm -rf files/etc/config/wireless
+rm -rf files/etc/modules.d/wireless_enable
 # Add luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git feeds/luci/themes/luci-theme-opentomcat
