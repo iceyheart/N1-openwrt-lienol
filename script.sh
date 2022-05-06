94f24dd68f65fcbed0c121f02e557f628f7dc884
iceyheart <40313247+iceyheart@users.noreply.github.com>
2022-05-06 15:41:54 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,9 +4,4 @@ cd openwrt
 #rm -rf files/etc/config/wireless
 #rm -rf files/etc/modules.d/wireless_enable
 # Add luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
-rm -rf theme-temp/luci-theme-opentomcat/LICENSE
-rm -rf theme-temp/luci-theme-opentomcat/README.md
-mv -f theme-temp/luci-theme-opentomcat package/lean/
-rm -rf theme-temp
-default_theme='opentomcat'
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git feeds/luci/themes/luci-theme-opentomcat
